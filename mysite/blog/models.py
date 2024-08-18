4d09573230b7e07d11611090b79201c23fc9854d
Dmitry <dmitry1298@yandex.ru>
2024-08-18 13:33:02 +1000

    creation of model managers

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -21,6 +27,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                                choices=Status.choices,
                                default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
@@ -30,4 +38,3 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
-    
